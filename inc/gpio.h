c09990068c0dc02b82be69c3be5b34b22b8f0eed
solawc <1225093964@qq.com>
2024-05-12 23:54:32 +0800

    gpio fix

--- inc/gpio.h
+++ inc/gpio.h
@@ -1,8 +1,7 @@
 #ifndef __gpio_h
 #define __gpio_h
 
-#include "stdint.h"
-#include "stm32f4xx_hal.h"
+#include "stdint.h" // uint32_t
 
 #define GPIO(PORT, NUM) (((PORT)-'A') * 16 + (NUM))       
 #define GPIO2PORT(PIN) ((PIN) / 16)                     // 通过PIN来计算当前属于GPIOX X端口的端口号
@@ -11,20 +10,27 @@
 // 编译相关
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
+
+void gpio_all_clock_init(void);
+
 struct gpio_out {
     void *gpio_port;
     uint16_t bit;
 };
+struct gpio_out gpio_out_setup(uint32_t pin, uint32_t val);
+void gpio_out_write(struct gpio_out g, uint32_t val);
+void gpio_out_toggle_noirq(struct gpio_out g);
+void gpio_out_toggle(struct gpio_out g);
 
 struct gpio_in {
     void *gpio_port;
     uint16_t bit;
 };
+struct gpio_in gpio_in_setup(uint32_t pin, uint32_t val);
+uint8_t gpio_in_read(struct gpio_in g);
 
-int regs_to_pin(GPIO_TypeDef  *regs, uint32_t bit);
-struct gpio_out gpio_out_setup(uint32_t pin, uint32_t val);
 
-
+/* for test function */
 void goio_test_init(void);
 void gpio_test_run(void);
 
--- src/gpio.c
+++ src/gpio.c
@@ -1,5 +1,5 @@
 #include "gpio.h"
-
+#include "stm32f4xx_hal.h" 
 
 GPIO_TypeDef  * const digital_regs[]={
     GPIOA,
@@ -38,43 +38,56 @@ GPIO_TypeDef  * const digital_regs[]={
 #endif
 };
 
-int ffs(int num) {
-    // 如果num为0，则返回0
-    if (num == 0) return 0;
+void gpio_all_clock_init(void) {
 
-    // 使用位运算找到最低位的设置位的位置
-    int position = 1;
-    while ((num & 1) == 0) {
-        num >>= 1;
-        position++;
-    }
-    return position;
+    __HAL_RCC_GPIOA_CLK_ENABLE(); 
+    __HAL_RCC_GPIOB_CLK_ENABLE(); 
+    __HAL_RCC_GPIOC_CLK_ENABLE(); 
+
+#ifdef GPIOD
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+#endif
+
+#ifdef GPIOE
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+#endif
+
+#ifdef GPIOF
+    __HAL_RCC_GPIOF_CLK_ENABLE();
+#endif
+
+#ifdef GPIOG
+    __HAL_RCC_GPIOG_CLK_ENABLE();
+#endif
+
+#ifdef GPIOH
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+#endif
+
+#ifdef GPIOI
+    __HAL_RCC_GPIOI_CLK_ENABLE();
+#endif
+
+#ifdef GPIOJ
+    __HAL_RCC_GPIOJ_CLK_ENABLE();
+#endif
+
+#ifdef GPIOJ
+    __HAL_RCC_GPIOK_CLK_ENABLE();
+#endif
 }
 
 
-int regs_to_pin(GPIO_TypeDef  *regs, uint32_t bit) {
-	int i;
-    for (i=0; i<ARRAY_SIZE(digital_regs); i++)
-        if (digital_regs[i] == regs)
-            return GPIO('A' + i, ffs(bit)-1);
-    return 0;
-}
 
 // 校验GPIO端口和pin引脚是否和芯片复合
 static int
 gpio_valid(uint32_t pin)
 {
     uint32_t port = GPIO2PORT(pin);
-
-    int array_size = ARRAY_SIZE(digital_regs);
-
-    GPIO_TypeDef *gpio_port = digital_regs[port];
-
-    // return port < ARRAY_SIZE(digital_regs) && digital_regs[port];
-    return port < array_size && digital_regs[port];
+    return port < ARRAY_SIZE(digital_regs) && digital_regs[port];
 }
 
-static void gpio_out_init(GPIO_TypeDef  *GPIOx ,uint32_t gpio_pin, uint32_t val) {
+void gpio_out_reset(GPIO_TypeDef  *GPIOx ,uint32_t gpio_pin, uint32_t val) {
 
     GPIO_InitTypeDef GPIO_Init;
 
@@ -90,9 +103,6 @@ static void gpio_out_init(GPIO_TypeDef  *GPIOx ,uint32_t gpio_pin, uint32_t val)
 struct gpio_out 
 gpio_out_setup(uint32_t pin, uint32_t val) {
 
-    // HAL CLK Init
-    // 在stm32的底层接口上，基本使用HAL库，方便迁移。
-
     int gpio_valid_get = gpio_valid(pin);
 
     if(!gpio_valid_get) {
@@ -104,11 +114,9 @@ gpio_out_setup(uint32_t pin, uint32_t val) {
 
     uint32_t bit = GPIO2BIT(pin);
 
-    gpio_out_init(gpio_port, bit, val);  // 真正初始化的地方
+    gpio_out_reset(gpio_port, bit, val);  // 真正初始化的地方
 
     struct gpio_out g = {.gpio_port=gpio_port,.bit=bit}; //记录并返回对应的GPIO信息
-
-    // GPIO_PIN_10 // 0X0400
 
     return g;
 }
@@ -118,47 +126,87 @@ void gpio_out_write(struct gpio_out g, uint32_t val) {
     HAL_GPIO_WritePin(g.gpio_port, g.bit, (GPIO_PinState)val);
 }
 
-struct gpio_in
-gpio_in_setup(uint32_t pin, uint32_t val) {
+void gpio_out_toggle_noirq(struct gpio_out g)
+{
+    HAL_GPIO_TogglePin(g.gpio_port, g.bit);
+}
 
-    if(gpio_valid(pin)) {
-        // error!;
-        while(1);
-    }
+void gpio_out_toggle(struct gpio_out g)
+{
+    // irqstatus_t flag = irq_save();
+    gpio_out_toggle_noirq(g);
+    // irq_restore(flag);
+}
+
+
+void gpio_in_reset(GPIO_TypeDef  *GPIOx ,uint32_t gpio_pin, uint32_t val) {
+
+    GPIO_InitTypeDef GPIO_Init;
+
+    HAL_GPIO_WritePin(GPIOx, gpio_pin, (GPIO_PinState)val);
+
+    GPIO_Init.Mode = GPIO_MODE_INPUT;
+    GPIO_Init.Pin = gpio_pin;
+    GPIO_Init.Pull = GPIO_NOPULL;
+    GPIO_Init.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    HAL_GPIO_Init(GPIOx, &GPIO_Init);
+}
+
+struct gpio_in gpio_in_setup(uint32_t pin, uint32_t val) {
+
+    if(!gpio_valid(pin)) { while(1); }  // 使用了不存在的引脚
 
     GPIO_TypeDef  *gpio_port = digital_regs[GPIO2PORT(pin)];
 
     uint32_t bit = GPIO2BIT(pin);
 
-    gpio_out_init(gpio_port, bit, val);  // 真正初始化的地方
+    gpio_in_reset(gpio_port, bit, val);  // 真正初始化的地方
 
     struct gpio_in g = {.gpio_port=gpio_port,.bit=bit}; //记录并返回对应的GPIO信息
 
     return g;
-
 }
 
+uint8_t gpio_in_read(struct gpio_in g)
+{
+    return (uint8_t)HAL_GPIO_ReadPin(g.gpio_port, g.bit);
+}
+
+/*******************使用demo***********************/
+
+#define LED_R_GPIO_PIN		GPIO('H', 10) 
+#define LED_G_GPIO_PIN		GPIO('H', 11)
+#define LED_B_GPIO_PIN		GPIO('H', 12) 
+
 struct test_io {
-
-	struct gpio_out test_io0;
-
+	struct gpio_out test_led_r, test_led_g, test_led_b;
 };
 
 struct test_io test_io_obj;
 
-#define LED_GPIO_PIN		GPIO('H', 11)   //regs_to_pin(GPIOH, 10)
-
 void goio_test_init(void) {
 
-	test_io_obj.test_io0 = gpio_out_setup(LED_GPIO_PIN, 0);
+	test_io_obj.test_led_r = gpio_out_setup(LED_R_GPIO_PIN, 0);
+    test_io_obj.test_led_g = gpio_out_setup(LED_G_GPIO_PIN, 0);
+    test_io_obj.test_led_b = gpio_out_setup(LED_B_GPIO_PIN, 0);
 }
 
 void gpio_test_run(void) {
 
 	while(1) {
-		gpio_out_write(test_io_obj.test_io0, 0);
+		gpio_out_write(test_io_obj.test_led_r, 0);
         HAL_Delay(500);
-		gpio_out_write(test_io_obj.test_io0, 1);
+		gpio_out_write(test_io_obj.test_led_r, 1);
+		HAL_Delay(500);
+
+        gpio_out_write(test_io_obj.test_led_g, 0);
+        HAL_Delay(500);
+		gpio_out_write(test_io_obj.test_led_g, 1);
+		HAL_Delay(500);
+
+        gpio_out_write(test_io_obj.test_led_b, 0);
+        HAL_Delay(500);
+		gpio_out_write(test_io_obj.test_led_b, 1);
 		HAL_Delay(500);
 	}
 }
