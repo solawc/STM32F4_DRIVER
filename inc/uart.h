8969903d7cb89f1cb6715ca701750f3ed80308f4
solawc <1225093964@qq.com>
2024-06-09 14:56:38 +0800

    change

--- inc/uart.h
+++ inc/uart.h
@@ -8,31 +8,56 @@
 #define RX_BUFFER_SIZE  192
 #define TX_BUFFER_SIZE  96
 
-struct uart_serial {
-    uint32_t baud;
-    UART_HandleTypeDef huart;
+// struct uart_serial {
+//     uint32_t baud;
+//     UART_HandleTypeDef huart;
 
-    uint8_t receive_buff[RX_BUFFER_SIZE], receive_pos;
-    uint8_t transmit_buff[TX_BUFFER_SIZE], transmit_pos, transmit_max;
+//     uint8_t receive_buff[RX_BUFFER_SIZE], receive_pos;
+//     uint8_t transmit_buff[TX_BUFFER_SIZE], transmit_pos, transmit_max;
+// };
+
+
+// struct uart_serial uart_init(uint32_t tx_pin, uint32_t rx_pin, void *uart_port, uint32_t baud);
+// void uart_write_byte(struct uart_serial uart, uint8_t data);
+// void uart_irq_enable(struct uart_serial uart);
+// void uart_rx_irq_enable(struct uart_serial uart);
+// void uart_tx_irq_enable(struct uart_serial uart);
+// void uart_rx_irq_disable(struct uart_serial uart);
+// void uart_tx_irq_disable(struct uart_serial uart);
+
+
+struct uart_data {
+    /* UART 句柄*/
+    UART_HandleTypeDef *handle;
+
+    /* UART 对应的GPIO */
+    GPIO_TypeDef *gpio;
+    uint16_t tx_pin;
+    uint16_t rx_pin;
+
+    IRQn_Type irq_n;
 };
 
 
 
+struct uart_device {
+    char *name;
+    struct uart_data *data;
+    int (*dev_uart_init)(struct uart_device *p_dev, uint32_t baud);
+    int (*dev_uart_send)(struct uart_device *p_dev, uint8_t data);
+    int (*dev_uart_recv)(struct uart_device *p_dev, uint8_t data);
+    int (*dev_uart_tx_irq)(struct uart_device *p_dev, bool status);
+    int (*dev_uart_rx_irq)(struct uart_device *p_dev, bool status);
+    int (*dev_uart_irq_set)(struct uart_device *p_dev, uint32_t PreemptPriority, uint32_t SubPriority);
+};
 
 
 
+struct uart_device *get_uart_dev(char *name);
+
+void uart_test1(void);
 
 
 
-struct uart_serial uart_init(uint32_t tx_pin, uint32_t rx_pin, void *uart_port, uint32_t baud);
-void uart_write_byte(struct uart_serial uart, uint8_t data);
-void uart_irq_enable(struct uart_serial uart);
-void uart_rx_irq_enable(struct uart_serial uart);
-void uart_tx_irq_enable(struct uart_serial uart);
-void uart_rx_irq_disable(struct uart_serial uart);
-void uart_tx_irq_disable(struct uart_serial uart);
-
-void serial_test_init(uint32_t baud);
-void serial_test(void);
 
 #endif
--- src/uart.c
+++ src/uart.c
@@ -1,133 +1,60 @@
 #include "gpio.h"
 #include "uart.h"
 
-static void uart_gpio_reset(uint32_t tx_pin, uint32_t rx_pin) {
-
-    GPIO_InitTypeDef GPIO_Init;
-
-    GPIO_TypeDef *tx_port = digital_regs[GPIO2PORT(tx_pin)];
-    GPIO_TypeDef *rx_port = digital_regs[GPIO2PORT(rx_pin)];
-
-    uint16_t tx_bit = GPIO2BIT(tx_pin);
-    uint16_t rx_bit = GPIO2BIT(rx_pin);
-
-    GPIO_Init.Mode = GPIO_MODE_AF_PP;
-    GPIO_Init.Pin = tx_bit;
-    GPIO_Init.Pull = GPIO_NOPULL;
-    GPIO_Init.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_Init.Alternate = (uint8_t)0x07; 
-    HAL_GPIO_Init(tx_port, &GPIO_Init);
-
-    GPIO_Init.Mode = GPIO_MODE_AF_PP;
-    GPIO_Init.Pin = rx_bit;
-    GPIO_Init.Pull = GPIO_NOPULL;
-    GPIO_Init.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_Init.Alternate = (uint8_t)0x07; 
-    HAL_GPIO_Init(rx_port, &GPIO_Init);
-}
-
-static struct uart_serial uart_config(void *uart_port, uint32_t baud) {
-
-    struct uart_serial uart;
-
-    uart.huart.Instance = uart_port;
-    uart.huart.Init.BaudRate = baud;
-    uart.huart.Init.WordLength = UART_WORDLENGTH_8B;
-    uart.huart.Init.StopBits = UART_STOPBITS_1;
-    uart.huart.Init.Parity = UART_PARITY_NONE;
-    uart.huart.Init.Mode = UART_MODE_TX_RX;
-    uart.huart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    uart.huart.Init.OverSampling = UART_OVERSAMPLING_16;
-    if (HAL_UART_Init(&uart.huart) != HAL_OK)
-    {
-			while(1);
-    }
-		
-	return uart;
-}
-
-struct uart_serial uart_init(uint32_t tx_pin, uint32_t rx_pin, void *uart_port, uint32_t baud) {
-
-    uart_gpio_reset(tx_pin, rx_pin);
-
-    return uart_config(uart_port, baud);
-}
-
-void uart_write_byte(struct uart_serial uart, uint8_t data) {
-
-    HAL_UART_Transmit(&(uart.huart), &data, 1, 1000);
-}
-
-void uart_write(struct uart_serial uart, uint8_t *str, uint16_t size) {
-
-    HAL_UART_Transmit(&(uart.huart), str, size, 0xffff);
-}
-
-void uart_irq_enable(struct uart_serial uart) {
-
-    HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
-	HAL_NVIC_EnableIRQ(USART1_IRQn);
-}
-
-void uart_rx_irq_enable(struct uart_serial uart) {
-
-    __HAL_UART_ENABLE_IT(&uart.huart, UART_IT_RXNE);
-}
-
-void uart_tx_irq_enable(struct uart_serial uart) {
-
-    __HAL_UART_ENABLE_IT(&uart.huart, UART_IT_TXE);
-}
-
-void uart_rx_irq_disable(struct uart_serial uart) {
-
-    __HAL_UART_DISABLE_IT(&uart.huart, UART_IT_RXNE);
-}
-
-void uart_tx_irq_disable(struct uart_serial uart) {
-
-    __HAL_UART_DISABLE_IT(&uart.huart, UART_IT_TXE);
-}
 
 
-/*****************************************************/
 
-/*****************demo********************************/
-#define UART1_TX    GPIO('A', 9) 
-#define UART1_RX    GPIO('A', 10) 
-#define UART1_GPIO_CLK_ENABLE()     __HAL_RCC_GPIOA_CLK_ENABLE()
-#define UART1_CLK_ENABLE()          __HAL_RCC_USART1_CLK_ENABLE()
+// struct uart_serial uart_init(uint32_t tx_pin, uint32_t rx_pin, void *uart_port, uint32_t baud) {
 
-struct uart_serial test_uart;
+//     uart_gpio_reset(tx_pin, rx_pin);
 
-void serial_test_init(uint32_t baud) {
+//     return uart_config(uart_port, baud);
+// }
 
-    UART1_GPIO_CLK_ENABLE();
-    UART1_CLK_ENABLE();
+// void uart_write_byte(struct uart_serial uart, uint8_t data) {
 
-    test_uart = uart_init(UART1_TX, UART1_RX, USART1, baud);
-}
+//     HAL_UART_Transmit(&(uart.huart), &data, 1, 1000);
+// }
 
-HAL_StatusTypeDef STATUS = HAL_ERROR;
+// void uart_write(struct uart_serial uart, uint8_t *str, uint16_t size) {
 
-void serial_test_send(uint8_t data) {
+//     HAL_UART_Transmit(&(uart.huart), str, size, 0xffff);
+// }
 
-    STATUS = HAL_UART_Transmit(&(test_uart.huart), &data, 1, 1000);
+// void uart_irq_enable(struct uart_serial uart) {
 
-    if(STATUS != HAL_OK) {
+//     HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+// 	   HAL_NVIC_EnableIRQ(USART1_IRQn);
+// }
 
-        while(1);
-    }
-}
+// void uart_rx_irq_enable(struct uart_serial uart) {
 
-void serial_test(void) {
+//     __HAL_UART_ENABLE_IT(&uart.huart, UART_IT_RXNE);
+// }
 
-    uint8_t str[9] = "GRBL DEBUG\r\n";
+// void uart_tx_irq_enable(struct uart_serial uart) {
 
-    uart_write(test_uart, str, 9);
+//     __HAL_UART_ENABLE_IT(&uart.huart, UART_IT_TXE);
+// }
 
-    HAL_Delay(500);
-}
+// void uart_rx_irq_disable(struct uart_serial uart) {
+
+//     __HAL_UART_DISABLE_IT(&uart.huart, UART_IT_RXNE);
+// }
+
+// void uart_tx_irq_disable(struct uart_serial uart) {
+
+//     __HAL_UART_DISABLE_IT(&uart.huart, UART_IT_TXE);
+// }
+/*
+
+    HAL_NVIC_SetPriority(LaserUART_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(LaserUART_IRQn);
+
+
+*/
+
+
 /***********************************************************************************************************************/
 
 UART_HandleTypeDef huart1 = {
@@ -141,25 +68,10 @@ UART_HandleTypeDef huart1 = {
     .Init.WordLength = UART_WORDLENGTH_8B,
 };
 
-struct uart_data {
-    /* UART 句柄*/
-    UART_HandleTypeDef *handle;
-
-    /* UART 对应的GPIO */
-    GPIO_TypeDef *gpio;
-    uint16_t tx_pin;
-    uint16_t rx_pin;
-};
-
-struct uart_device {
-    char *name;
-    struct uart_data *data;
-    int (*dev_uart_init)(struct uart_device *p_dev, uint32_t baud);
-    int (*dev_uart_send)(struct uart_device *p_dev, uint8_t data);
-    int (*dev_uart_recv)(struct uart_device *p_dev, uint8_t data);
-    int (*dev_uart_tx_irq)(struct uart_device *p_dev, bool status);
-    int (*dev_uart_rx_irq)(struct uart_device *p_dev, bool status);
-};
+static int dev_uart_irq_enable(struct uart_device *p_dev, uint32_t PreemptPriority, uint32_t SubPriority) {
+    HAL_NVIC_SetPriority(p_dev->data->irq_n, PreemptPriority, SubPriority);
+    HAL_NVIC_EnableIRQ(p_dev->data->irq_n);
+}
 
 static int dev_uart_gpio_init(struct uart_device *p_dev) {
 
@@ -169,7 +81,7 @@ static int dev_uart_gpio_init(struct uart_device *p_dev) {
     GPIO_Init.Pull = GPIO_NOPULL;
     GPIO_Init.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_Init.Alternate = (uint8_t)0x07; 
-    HAL_GPIO_Init(p_dev->data->gpio, p_dev->data->handle);
+    HAL_GPIO_Init(p_dev->data->gpio, &GPIO_Init);
 }
 
 static int dev_uart_init(struct uart_device *p_dev, uint32_t baud)
@@ -178,8 +90,11 @@ static int dev_uart_init(struct uart_device *p_dev, uint32_t baud)
 
     dev_uart_gpio_init(p_dev);
 
+    data->handle->Init.BaudRate = baud; 
+
     if (HAL_UART_Init(data->handle) != HAL_OK)
     {
+        while(1);
 		return -1;
     }
 
@@ -188,9 +103,14 @@ static int dev_uart_init(struct uart_device *p_dev, uint32_t baud)
 
 static int dev_uart_send(struct uart_device *p_dev, uint8_t data)
 {
-    HAL_UART_Transmit(p_dev->data->handle, data, 1, 100);
+    HAL_StatusTypeDef status = HAL_OK;
 
-    return 0;
+    status = HAL_UART_Transmit(p_dev->data->handle, &data, 1, 100);
+
+    if(status != HAL_OK) 
+        return -1;
+    else 
+        return 0;
 }
 
 static int dev_uart_recv(struct uart_device *p_dev, uint8_t *data)
@@ -205,7 +125,6 @@ int dev_uart_tx_irq(struct uart_device *p_dev, bool status)
         __HAL_UART_ENABLE_IT(p_dev->data->handle, UART_IT_TXE);
     else 
         __HAL_UART_DISABLE_IT(p_dev->data->handle, UART_IT_TXE);
-
     return 0;
 }
 
@@ -224,6 +143,7 @@ struct uart_data g_stm32_uart1_data = {
     GPIOA,
     GPIO_PIN_9,
     GPIO_PIN_10,
+    USART1_IRQn,
 };
 
 struct uart_device uart1 = {
@@ -234,5 +154,41 @@ struct uart_device uart1 = {
     dev_uart_recv,
     dev_uart_tx_irq,
     dev_uart_rx_irq,
+    dev_uart_irq_enable,
 };
 
+struct uart_device *g_uart_dev_num[] = {
+    &uart1,
+};
+
+struct uart_device *get_uart_dev(char *name) {
+
+    int i = 0;
+
+    for(i=0; i< sizeof(g_uart_dev_num) / sizeof(g_uart_dev_num[0]); i++) {
+
+        if( 0 == strcmp(name, g_uart_dev_num[i]->name)) {
+            return g_uart_dev_num[i];
+        }
+    }
+
+    return NULL;
+}
+
+void uart_test1(void) {
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_USART1_CLK_ENABLE();
+
+    struct uart_device *p_dev;
+    p_dev = get_uart_dev("stm32_uart1");
+
+    p_dev->dev_uart_init(p_dev, 115200);
+
+    while (1)
+    {
+        p_dev->dev_uart_send(p_dev, 'B');
+        HAL_Delay(500);
+    }
+
+}
